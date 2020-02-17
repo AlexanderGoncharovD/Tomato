ea8e7dc5ce26aebcffb7e1870e0eb868c574e9ed
PenguinLAB <goncharov54338@gmail.com>
2020-02-17 21:47:38 +0300

    TM-1 Delete pictures. Add timer circle

--- app/Tomato/MainActivity.cs
+++ app/Tomato/MainActivity.cs
@@ -5,27 +5,30 @@ using Android.Runtime;
 using Android.Widget;
 using System;
 using Android.Views;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
 
 namespace Tomato
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         private TextView lbl;
         private TomatoTimer.TomatoTimer Timer;
+        private bool _isVibration = false;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
+            Platform.Init(this, savedInstanceState);
+
             SetContentView(Resource.Layout.activity_main);
 
             var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetActionBar(toolbar);
-            ActionBar.Title = "My Toolbar";
+            SetStatusBarColor();
             
-
             lbl = FindViewById<TextView>(Resource.Id.lbl);
             var start = FindViewById<Button>(Resource.Id.start_button);
             var pause = FindViewById<Button>(Resource.Id.pause_button);
@@ -35,13 +38,36 @@ namespace Tomato
             pause.Click += Pause_Click;
             stop.Click += Stop_Click;
 
+            var settings = FindViewById<ImageButton>(Resource.Id.settings_button);
+            settings.Click += SettingsButton_Click;
+
             Timer = new TomatoTimer.TomatoTimer(90);
             Timer.TimerTick += OnTimerTick;
+            Timer.TimerLeft += OnTimerLeft;
+
         }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        #region Private Methods
 
         private void Stop_Click(object sender, EventArgs e)
         {
-            Timer.Stop();
+            if (_isVibration)
+            {
+                _isVibration = false;
+                Xamarin.Essentials.Vibration.Cancel();
+            }
+            else
+            {
+                Timer.Stop();
+            }
         }
 
         private void Pause_Click(object sender, EventArgs e)
@@ -54,30 +80,60 @@ namespace Tomato
             Timer.Start();
         }
 
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
+        /// <summary>
+        ///     Обновление тика таймера
+        /// </summary>
         private void OnTimerTick(object sender, EventArgs e)
         {
             lbl.Text = $"{Timer.Minutes}:{Timer.Seconds}";
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        /// <summary>
+        ///     Обработичк завершения таймера
+        /// </summary>
+        private void OnTimerLeft(object sender, EventArgs e)
         {
-            MenuInflater.Inflate(Resource.Menu.top_menus, menu);
-            return base.OnCreateOptionsMenu(menu);
+            Vibration(TimeSpan.FromSeconds(1));
         }
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
+        /// <summary>
+        ///     Устанавливает цвет статус бара
+        /// </summary>
+        private void SetStatusBarColor()
         {
-            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
-                ToastLength.Short).Show();
-            return base.OnOptionsItemSelected(item);
+            var color = ColorConverters.FromHex("#1B3147").ToPlatformColor();
+
+            Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+            Window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+            Window.SetStatusBarColor(new Android.Graphics.Color(color));
         }
+
+        /// <summary>
+        ///     Обработчик кнопки настроект
+        /// </summary>
+        private void SettingsButton_Click(object secnder, EventArgs e)
+        {
+            Toast.MakeText(this, "Action selected: Settings",
+                ToastLength.Short).Show();
+        }
+
+        /// <summary>
+        ///     Вибрация продолжительностью <paramref name="time"/>
+        /// </summary>
+        /// <param name="time">
+        ///     Время продолжительности вибрации
+        /// </param>
+        private async void Vibration(TimeSpan time)
+        {
+            _isVibration = true;
+            while (_isVibration)
+            {
+                Xamarin.Essentials.Vibration.Vibrate(time);
+                await Task.Delay(1500);
+            }
+        }
+
+        #endregion
 
     }
 }
